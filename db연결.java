c07a180c45a6fd1045d9c08ca464a07980450433
Hanmaesil <93114691+Hanmaesil@users.noreply.github.com>
2021-12-09 18:04:37 +0900

    자바와 데이터베이스 연결 이론

---
+++ db연결.java
@@ -0,0 +1,20 @@
+
+public class db연결 {
+	// 프로그램과 데이터베이스를 connection을 통해 연결한다.(sql에서 conn은 커넥션을 연결하는 명령어다.)
+	// driver를 통해 connection(통로)를 만들어준다.
+	// driver를 설치할땐 dbms에 알맞는 드라이버를 설치해야 한다.
+
+	// DBMS : 데이터베이스를 관리해준다.(EX - 오라클,MYSQL 등등등)
+
+	// db연결 순서
+	// 1.드라이버설치
+	// 2.통로 생성
+	// 3.sql문 준비 및 실행.
+	// 4. 연결종료
+	
+	//자바와 데이터베이스 연결 기법 - >>>>> JDBC!
+	
+	//자바 안에서 오라클 사용하기 : 메뉴창의 윈도우에서 showview - other - date검색 - data source explorer  open
+	
+
+}
